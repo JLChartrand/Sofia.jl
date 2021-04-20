542bb353321f18f1f9fe88ecb374d12db375f510
jean_lc <jl_chartrand@hotmail.com>
2021-04-20 15:15:05 -0400

    Update code.jl

--- src/code.jl
+++ src/code.jl
@@ -13,6 +13,6 @@ function H!(x::Vector{T}, mo::Model, stack::Matrix)
     return mo.H!(x, stack)
 end
 
-function Hdotv!(x::Vector{T}, mo::Model)
-    return mo.F(x)
+function Hdotv!(x::Vector{T}, mo::Model, v::Vector, stack::Vector)
+    return mo.Hdotv(x, v, stack)
 end
