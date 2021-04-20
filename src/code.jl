30b496e3a99199894d9c394cfe4e8c256dc7c4f6
jean_lc <jl_chartrand@hotmail.com>
2021-04-20 15:18:34 -0400

    Update code.jl

--- src/code.jl
+++ src/code.jl
@@ -1,4 +1,4 @@
-F, grad!, H!, Hdotv!, BHHH!, BHHHdotv!, Fs, grads!
+#F, grad!, H!, Hdotv!, BHHH!, BHHHdotv!, Fs, grads!
 
 
 function F(x::Vector{T}, mo::Model)
@@ -15,4 +15,20 @@ end
 
 function Hdotv!(x::Vector{T}, mo::Model, v::Vector, stack::Vector)
     return mo.Hdotv(x, v, stack)
+end
+function BHHH!(x, mo::Model, stack::Matrix)
+    prinltn("BHHH! not defined for Model")
+    return zeros(1, 1)
+end
+function BHHHdotv!(x, mo::Model, v::Vector, stack::Vector)
+    prinltn("BHHHdot! not defined for Model")
+    return zeros(1, 1)
+end
+function Fs(x::Vector, mo::Model)
+    prinltn("Fs not defined for Model")
+    return zeros(1)
+end
+function grads!(x::Vector{T}, mo::Model, stack::Matrix)
+    prinltn("grads! not defined for Model")
+    return zeros(1, 1)
 end
