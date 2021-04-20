ff8c049acc8dc52cbe37080cd6c1ac4dcb6952d8
jean_lc <jl_chartrand@hotmail.com>
2021-04-20 15:22:27 -0400

    Update code.jl

--- src/code.jl
+++ src/code.jl
@@ -1,26 +1,26 @@
 #F, grad!, H!, Hdotv!, BHHH!, BHHHdotv!, Fs, grads!
 
 
-function F(x::Vector{T}, mo::Model)
+function F(x::Vector, mo::Model)
     return mo.F(x)
 end
 
-function grad!(x::Vector{T}, mo::Model, stack::Vector)
+function grad!(x::Vector, mo::Model, stack::Vector)
     mo.grad!(x, stack)
 end
 
-function H!(x::Vector{T}, mo::Model, stack::Matrix)
+function H!(x::Vector, mo::Model, stack::Matrix)
     return mo.H!(x, stack)
 end
 
-function Hdotv!(x::Vector{T}, mo::Model, v::Vector, stack::Vector)
+function Hdotv!(x::Vector, mo::Model, v::Vector, stack::Vector)
     return mo.Hdotv(x, v, stack)
 end
-function BHHH!(x, mo::Model, stack::Matrix)
+function BHHH!(x::Vector, mo::Model, stack::Matrix)
     prinltn("BHHH! not defined for Model")
     return zeros(1, 1)
 end
-function BHHHdotv!(x, mo::Model, v::Vector, stack::Vector)
+function BHHHdotv!(x::Vector, mo::Model, v::Vector, stack::Vector)
     prinltn("BHHHdot! not defined for Model")
     return zeros(1, 1)
 end
@@ -28,7 +28,7 @@ function Fs(x::Vector, mo::Model)
     prinltn("Fs not defined for Model")
     return zeros(1)
 end
-function grads!(x::Vector{T}, mo::Model, stack::Matrix)
+function grads!(x::Vector, mo::Model, stack::Matrix)
     prinltn("grads! not defined for Model")
     return zeros(1, 1)
 end
