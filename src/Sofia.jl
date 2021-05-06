a2312835d3f00619c9eaae72b7ac923812910b8c
JeremyRieussec <jeremy.rieussec@gmail.com>
2021-05-06 17:56:15 -0400

    Update export

--- src/Sofia.jl
+++ src/Sofia.jl
@@ -1,7 +1,8 @@
 module Sofia
 using ForwardDiff
 
-export F, grad!, H!, Hdotv!, Hdotv, BHHH!, BHHHdotv!, BHHHdotv, Fs, grads!,
+export F, grad!, H!, Hdotv!, Hdotv, BHHH!, BHHHdotv!, BHHHdotv, Fs, grads!, grads,
+    gradind, gradind!, Find, 
     AbstractStochasticModel, AbstractModel, Model,
     IsUpdatable, Updatable, NotUpdatable, Nobs
 include("gen.jl")
