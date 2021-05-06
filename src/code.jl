d75ea2a2b9b32b6b843fad7973e34b781deb5495
JeremyRieussec <jeremy.rieussec@gmail.com>
2021-05-06 17:26:23 -0400

    Ajout gradind and Find

--- src/code.jl
+++ src/code.jl
@@ -1,13 +1,45 @@
 #F, grad!, H!, Hdotv, Hdotv!, BHHH!, BHHHdotv, BHHHdotv!, Fs, grads!
 
 
+# ---------------- Function -----------------------
 function F(x::Vector, mo::AbstractModel)
     return mo.F(x)
 end
 
+function Fs(x::Vector, mo::AbstractModel)
+    throw(ErrorException("Fs not defined for Model"))
+    return zeros(1)
+end
+
+function Find(x::Vector, mo::AbstractModel)
+    throw(ErrorException("Find not defined for Model"))
+    return nothing
+end
+
+# ------------------- gradient -----------------------
 function grad!(x::Vector, mo::AbstractModel, stack::Vector)
     mo.grad!(x, stack)
 end
+
+function grads(x::Vector, mo::AbstractModel)
+    throw(ErrorException("grads not defined for Model"))
+    return nothing
+end
+
+function grads!(x::Vector, mo::AbstractModel, stack::Array{Vector, 1})
+    throw(ErrorException("grads! not defined for Model"))
+end
+
+function gradind(x::Vector, mo::AbstractModel)
+    throw(ErrorException("gradind not defined for Model"))
+    return nothing
+end
+
+function gradind!(x::Vector, mo::AbstractModel, stack::Vector)
+    throw(ErrorException("gradind! not defined for Model"))
+end
+
+# ----------------- Hessian ---------------------------
 
 function H!(x::Vector, mo::AbstractModel, stack::Matrix)
     return mo.H!(x, stack)
@@ -21,27 +53,16 @@ function Hdotv!(x::Vector, mo::AbstractModel, v::Vector, stack::Vector)
     return mo.Hdotv!(x, v, stack)
 end
 
+# --------------------- BHHH -----------------------------
 function BHHH!(x::Vector, mo::AbstractModel, stack::Matrix)
-    prinltn("BHHH! not defined for Model")
-    #return  zeros(1, 1)
+    throw(ErrorException("BHHH! not defined for Model"))
 end
 
 function BHHHdotv(x::Vector, mo::AbstractModel, v::Vector)
-    prinltn("BHHHdot not defined for Model")
-    #return zeros(1)
-end
-
-function BHHHdotv!(x::Vector, mo::AbstractModel, v::Vector, stack::Vector)
-    prinltn("BHHHdot! not defined for Model")
-    #return zeros(1)
-end
-
-function Fs(x::Vector, mo::AbstractModel)
-    prinltn("Fs not defined for Model")
+    throw(ErrorException("BHHHdot not defined for Model"))
     return zeros(1)
 end
 
-function grads!(x::Vector, mo::AbstractModel, stack::Array{Vector, 1})
-    prinltn("grads! not defined for Model")
-    #return zeros(1, 1)
+function BHHHdotv!(x::Vector, mo::AbstractModel, v::Vector, stack::Vector)
+    throw(ErrorException("BHHHdot! not defined for Model"))
 end
